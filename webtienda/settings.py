0f6a69a58ca55e307caf2538a707a7b2ac8a26f0
JavierZXE <153473398+JavierZXE@users.noreply.github.com>
2024-07-02 04:52:05 -0400

    Add files via upload

--- webtienda/settings.py
+++ webtienda/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "usuarios",
     "productos",
     "carro",
-    "pedidos"
+    "pedidos",
+    "staff"
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
--- webtienda/urls.py
+++ webtienda/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.conf import settings
+from django.urls import path, include,re_path
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("productos.urls")),
     path("user/", include("usuarios.urls")),
+    path("carro/", include("carro.urls")),
     path("staff/", include("staff.urls"))
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,  
+    })
 ]
